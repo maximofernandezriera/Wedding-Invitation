e0dd03d622168caae302f475ee0a765ce520eb4f
Máximo Fernández Riera <43608040+maximofernandezriera@users.noreply.github.com>
2021-12-06 23:00:50 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
+var countDownDate = new Date("Dic 31, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -44,7 +44,7 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br><Días</div>" + "<div class='hours block'>" + hours + "<br>Horas</div>" + "<div class='minutes block'>" + minutes + "<br>Minutos</div>" + "<div class='seconds block'>" + seconds + "<br>Segundos</div></div>";
     
     // If the count down is over, write some text 
     if (distance < 0) {
